15804c48e64460b8d240890d1fc91c12ac605180
rafalwlo <rafal1112@gmail.com>
2015-04-05 22:08:19 +0200

    JS onclick und Funktionsnamen
    Und teilweise responsive design drinnen.

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,24 +1,23 @@
 var chosenButton = null;
 
-function lol(){
+function lol() {
 	alert("asdf");
 }
-/*
-function select(){
+
+function selectButton() {
 	alert("wtf");
 	var element = document.getElementById("tv1");
-	if(chosenButton === null){	
+	if(chosenButton === null) {	
 		chosenButton = element;
-		element.setAttribute("background", "linear-gradient(rgb(251,119,0), rgb(230,109,0))")
+		element.setAttribute("background", "linear-gradient(rgb(251,119,0), rgb(230,109,0));");
 	}
-	else{
-		deselect()
+	else {
+		deselectButton();
 	}
 }
 
-function deselect(){
-	chosenButton.setAttribute('disabled', 'disabled');
+function deselectButton() {
+	/* chosenButton.setAttribute('disabled', 'disabled'); */
+	/* NEIN, das gilt nur für komplett inaktive Buttons (graue), deselect soll sie wieder blau machen */
 	chosenButton = null;
 }
-
-*/
