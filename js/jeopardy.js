6d36865ba5c547a65cce85c7bc5269cdeb3378d0
MrStrelow <mathias.cammerlander@gmx.net>
2015-04-05 23:16:06 +0200

    script fuer auswahl, jepardy.xhtml anpassung fuer script

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,23 +1,70 @@
-var chosenButton = null;
+var chosenButtonTv = null;
+var chosenButtonSsd = null;
+var chosenButtonWeb = null;
+var chosenButtonSport = null;
+var chosenButtonTuwien = null;
 
-function lol() {
-	alert("asdf");
+function selectButtonTv(e){
+	var element = e.target || e.srcElement;
+	if(chosenButtonTv === null){	
+		chosenButtonTv = element;
+		element.setAttribute("class", "activeButton");
+	}
+	else{
+		chosenButtonTv.setAttribute("class", "inactiveButton");
+		element.setAttribute("class", "activeButton");
+		chosenButtonTv = element;
+	}
 }
 
-function selectButton() {
-	alert("wtf");
-	var element = document.getElementById("tv1");
-	if(chosenButton === null) {	
-		chosenButton = element;
-		element.setAttribute("background", "linear-gradient(rgb(251,119,0), rgb(230,109,0));");
+function selectButtonSsd(e){
+	var element = e.target || e.srcElement;
+	if(chosenButtonSsd === null){	
+		chosenButtonSsd = element;
+		element.setAttribute("class", "activeButton");
 	}
-	else {
-		deselectButton();
+	else{
+		chosenButtonSsd.setAttribute("class", "inactiveButton");
+		element.setAttribute("class", "activeButton");
+		chosenButtonSsd = element;
 	}
 }
 
-function deselectButton() {
-	/* chosenButton.setAttribute('disabled', 'disabled'); */
-	/* NEIN, das gilt nur für komplett inaktive Buttons (graue), deselect soll sie wieder blau machen */
-	chosenButton = null;
+function selectButtonWeb(e){
+	var element = e.target || e.srcElement;
+	if(chosenButtonWeb === null){	
+		chosenButtonWeb = element;
+		element.setAttribute("class", "activeButton");
+	}
+	else{
+		chosenButtonWeb.setAttribute("class", "inactiveButton");
+		element.setAttribute("class", "activeButton");
+		chosenButtonWeb = element;
+	}
+}
+
+function selectButtonSport(e){
+	var element = e.target || e.srcElement;
+	if(chosenButtonSport === null){	
+		chosenButtonSport = element;
+		element.setAttribute("class", "activeButton");
+	}
+	else{
+		chosenButtonSport.setAttribute("class", "inactiveButton");
+		element.setAttribute("class", "activeButton");
+		chosenButtonSport = element;
+	}
+}
+
+function selectButtonTuwien(e){
+	var element = e.target || e.srcElement;
+	if(chosenButtonTuwien === null){	
+		chosenButtonTuwien = element;
+		element.setAttribute("class", "activeButton");
+	}
+	else{
+		chosenButtonTuwien.setAttribute("class", "inactiveButton");
+		element.setAttribute("class", "activeButton");
+		chosenButtonTuwien = element;
+	}
 }
